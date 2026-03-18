8f4e7826c0d68796b43a990182ab1f52f52ef079
patakrishna2006-a11y <patakrishna2006@gmail.com>
2026-03-18 19:56:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=True)
 
 class CalculationHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
